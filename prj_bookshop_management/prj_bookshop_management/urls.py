0aeff0fa51460c63d1fe36e038b7862efc26430b
giapdoan01 <gdoan012@gmail.com>
2025-11-09 12:49:14 +0700

    rm cached

--- prj_bookshop_management/prj_bookshop_management/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('books/', include('bookshop.urls')),
-    path('', include('authentication.urls')),
-]
