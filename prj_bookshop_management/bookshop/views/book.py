5dd3f6d46a4fca771fc09c1318f28e9e89054334
kingofforeverr <nguyetha1234hn@gmail.com>
2025-11-09 13:11:20 +0700

    fixx

--- prj_bookshop_management/bookshop/views/book.py
+++ prj_bookshop_management/bookshop/views/book.py
@@ -4,7 +4,7 @@ from ..models import Sach
 
 def sach_list(request):
     sach_list = Sach.objects.all()
-    return render(request, 'sach/sach_list.html', {'sach_list': sach_list})
+    return render(request, 'sach/danh_sach_sach.html', {'sach_list': sach_list})
 
 from ..forms import SachForm
 
@@ -16,7 +16,7 @@ def sach_create(request):
             return redirect('sach_list')
     else:
         form = SachForm()
-    return render(request, 'sach/sach_form.html', {'form': form})
+    return render(request, 'sach/them_sach.html', {'form': form})
 
 def sach_update(request, ma_sach):
     sach = get_object_or_404(Sach, ma_sach=ma_sach)
@@ -31,11 +31,11 @@ def sach_update(request, ma_sach):
         # Khi GET, hiển thị form với dữ liệu hiện tại
         form = SachForm(instance=sach)
     
-    return render(request, 'sach/sach_form.html', {'form': form})
+    return render(request, 'sach/them_sach.html', {'form': form})
 
 def sach_delete(request, ma_sach):
     sach = get_object_or_404(Sach, ma_sach=ma_sach)
     if request.method == 'POST':
         sach.delete()
         return redirect('sach_list')
-    return render(request, 'sach/sach_confirm_delete.html', {'sach': sach})
+    return render(request, 'sach/xac_nhan_xoa.html', {'sach': sach})
