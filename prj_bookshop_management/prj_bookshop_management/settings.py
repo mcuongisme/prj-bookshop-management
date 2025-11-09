fc1242f958d0def63c4beaa0aea2177d4b3a0615
Huynh Manh Cuong <126797694+mcuongisme@users.noreply.github.com>
2025-11-09 10:54:01 +0700

    Update ALLOWED_HOSTS for production deployment

--- prj_bookshop_management/prj_bookshop_management/settings.py
+++ prj_bookshop_management/prj_bookshop_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@v&$i#p&_!96p7+*7yfqe01#enp^p*0&-6srsqxa_&hw20ky6b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['prj-bookshop-management-1.onrender.com']
 
 
 # Application definition
