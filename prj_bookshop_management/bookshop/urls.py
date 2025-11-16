61c0dd8cc7691586f98f72e7ccaacff435978092
kingofforeverr <nguyetha1234hn@gmail.com>
2025-11-16 10:30:36 +0700

    full16/11

--- prj_bookshop_management/bookshop/urls.py
+++ prj_bookshop_management/bookshop/urls.py
@@ -1,23 +1,25 @@
 from django.urls import path
 from . import views
 from .views.book import sach_list, sach_create, sach_update, sach_delete
-from .views import list_hoadon, create_hoadon, update_hoadon, delete_hoadon
-from .views.customer import list_customer, create_customer, update_customer, delete_customer
+from .views.hoadon import donhang_list, donhang_them,donhang_sua, donhang_xoa
+from .views.customer import khachhang_list, khachhang_create, khachhang_update, khachhang_delete
 urlpatterns = [
     path('', views.home, name='book_index'),
     path('sach/', sach_list, name='sach_list'),
     path('sach/create/', sach_create, name='sach_create'),
     path('sach/update/<str:ma_sach>/', sach_update, name='sach_update'),
     path('sach/delete/<str:ma_sach>/', sach_delete, name='sach_delete'),
-    path('customer/', list_customer, name='khach_list'),
-    path('customer/create/', create_customer, name='them_khach'),
-    path('customer/update/<str:ma_sach>/', update_customer, name='sua_khach'),
-    path('customer/delete/<str:ma_sach>/', delete_customer, name='xoa_khach'),
     
-    path('hoadon/', list_hoadon, name='hoadon_list'),
-    path('hoadon/create/', create_hoadon, name='hoadon_create'),
-    path('hoadon/update/<str:ma_hoa_don>/', update_hoadon, name='hoadon_update'),
-    path('hoadon/delete/<str:ma_hoa_don>/', delete_hoadon, name='hoadon_delete'),
     
+    path('donhang/', donhang_list, name='donhang_list'),
+    path('donhang/them/', donhang_them, name='donhang_them'),
+    path('donhang/sua/<str:ma_hoa_don>/',donhang_sua, name='donhang_sua'),
+    path('donhang/xoa/<str:ma_hoa_don>/', donhang_xoa, name='donhang_xoa'),
+    
+    
+    path('khachhang/', khachhang_list, name='khachhang_list'),
+    path('khachhang/them/', khachhang_create, name='khachhang_create'),
+    path('khachhang/sua/<str:sdt>/', khachhang_update, name='khachhang_update'),
+    path('khachhang/xoa/<str:sdt>/', khachhang_delete, name='khachhang_delete'),
     path('bao-cao/', views.bao_cao_view,  name='bao_cao_index'),
 ]
--- prj_bookshop_management/bookshop/views/book.py
+++ prj_bookshop_management/bookshop/views/book.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from ..models import Sach
+from ..models import DonHang
+from ..forms import DonHangForm
+from ..forms import SachForm
 
 def sach_list(request):
-    sach_list = Sach.objects.all()
-    return render(request, 'sach/danh_sach_sach.html', {'sach_list': sach_list})
+    query = request.GET.get('q')  
+    if query:
+        sach_list = Sach.objects.filter(ten_sach__icontains=query)  
+    else:
+        sach_list = Sach.objects.all()
+    return render(request, 'sach/danh_sach_sach.html', {
+        'sach_list': sach_list,
+        'query': query 
+    })
 
-from ..forms import SachForm
 
 def sach_create(request):
     if request.method == 'POST':
@@ -39,3 +48,16 @@ def sach_delete(request, ma_sach):
         sach.delete()
         return redirect('sach_list')
     return render(request, 'sach/xac_nhan_xoa.html', {'sach': sach})
+
+
+# Thêm hóa đơn
+def donhang_them(request):
+    if request.method == "POST":
+        form = DonHangForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_hoadon')
+    else:
+        form = DonHangForm()
+
+    return render(request, 'them_hoadon.html', {'form': form})
